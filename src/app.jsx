9cc473628c43e5368fc0b6e2d298dacadf1f5a77
vic6 <vicmatthews116@gmail.com>
2017-12-05 15:53:01 -0800

    Set up components and dev server

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => <div>hello dude</div>;
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.jsx",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.jsx?$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    publicPath: '/public/'
+  }
+};
